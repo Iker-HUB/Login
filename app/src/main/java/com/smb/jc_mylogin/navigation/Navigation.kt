c3bde9c7f009a525eb559b30a3fa2ca9a97ce15e
Salvador <salvador.martos@gmail.com>
2024-10-14 18:45:08 +0200

    Initial commit

---
+++ app/src/main/java/com/smb/jc_mylogin/navigation/Navigation.kt
@@ -0,0 +1,31 @@
+package com.smb.jc_mylogin.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.core.splashscreen.SplashScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.smb.jc_mylogin.screens.SplashScreen
+import com.smb.jc_mylogin.screens.login.LoginScreen
+import com.smb.jc_mylogin.screens.home.Home
+
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Screens.SplashScreen.name
+    ) {
+        composable(Screens.SplashScreen.name){
+            SplashScreen(navController = navController)
+        }
+        composable(Screens.LoginScreen.name){
+            LoginScreen(navController = navController)
+        }
+        composable(Screens.HomeScreen.name){
+            Home(navController = navController)
+        }
+
+    }
+}
